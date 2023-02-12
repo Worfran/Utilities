b8bdfe6a4c82808c39938bbf2a699498fccd93ce
worfran <fraworman@hotmail.com>
2023-02-12 17:25:09 -0500

    Actividad 3 completa

--- Codigos/Analisis de datos/Analisis rayos X.py
+++ Codigos/Analisis de datos/Analisis rayos X.py
@@ -3,8 +3,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+from scipy.optimize import curve_fit
+from scipy import stats
+
 sns.set_style('darkgrid')
 #%%
 __Ipath__= "C:/Users/frawo\Documents/Programacion/Utilities/Images/"
@@ -12,6 +13,11 @@ __Ipath__= "C:/Users/frawo\Documents/Programacion/Utilities/Images/"
 """
 Funciones Auxiliares
 """
+
+eV= 1000*1.602E-19
+
+def maximos(x):
+    return None 
 
 def Bragg(theta,d,n):
     factor1 = 2*d* np.sin(theta)
@@ -23,6 +29,18 @@ def errorS1(d,x:any):
 
 def normlog(y,y_0):
     return np.log(y/y_0)
+
+def fun(x,a,b,c,d):
+    factor = a*np.power(x,3)+b*np.power(x,2)+c*np.power(x,1)+d*np.power(x,0)
+    return factor
+
+def fun2(x,a):
+    factor1 =a*1*np.sqrt(np.power((x-9.979),3))
+    return factor1
+
+def fun3(x,a):
+    factor1 =a*1*np.sqrt(np.power(x,3))
+    return factor1
 
 #%%
 """
@@ -50,14 +68,12 @@ derror = pd.DataFrame()
 
 derror["ERROR"] =  10E9*err
 
-
 #%%
 
 """
 Graficacion 
 """
-#eliminar estas comillas para generar graficas
-"""
+
 _, axes = plt.subplots(1,2, figsize = (15, 5))
 
 axs1 = axes[0]
@@ -82,8 +98,6 @@ axs2.errorbar(df["Longitud de onda"], df["Intensidad"] , yerr=derror["ERROR"], f
 
 _.savefig(__Ipath__+'Actividad 1.png',dpi=1200)
 
-"""
-#eliminar estas comillas para generar graficas
 # %%
 """
 Actividad 2
@@ -185,8 +199,7 @@ df11= pd.DataFrame(d)
 """
 Graficar A22
 """
-#eliminar estas comillas para generar graficas
-"""
+
 plt.figure()
 sns.scatterplot(data=df11, 
     x="Espesor", y="Intensidad", 
@@ -196,47 +209,46 @@ plt.xlabel("grosor en mm")
 plt.ylabel("Intensidad +/- 1 (Imp/s)")
 plt.title("Intensidad de cada longitud de onda para distintos espesores")
 plt.savefig(__Ipath__ + "Actividad 2-2.png",dpi=1200)
-"""
-#eliminar estas comillas para generar graficas
+
 
 # %%
 """
 Regresiones lineales
 """
 lam = df11["Longitud de onda"].iloc[0:9]
-x0 = np.array(df11["Espesor"][df11["Longitud de onda"] == lam[0]]).reshape(-1,1)
+x0 = np.array(df11["Espesor"][df11["Longitud de onda"] == lam[0]])
 
 Mus = np.zeros(len(lam))
+Mus_error = np.zeros(len(lam))
 
 for i in range(len(lam)):
     yi = np.array(df11["Intensidad"][df11["Longitud de onda"] == lam[i]])
-    model_i = LinearRegression().fit(x0,yi)
-    Mus[i] = model_i.coef_
+    model_i = stats.linregress(x0,yi)
+    Mus[i] = round(model_i.slope,2)
+    Mus_error[i] = round(model_i.stderr,2)
 
+p = 2.7E3
+x = 1E3 * np.power(np.array(lam),3)
 
+x_grid = np.arange(min(x),max(x),4.19E-3)
 
-x = 1E3*np.power(np.array(lam),3)
+Mus = Mus / p
+Mus_error = Mus_error / p
 
-
-d = {"Espesor": x, "Mus": Mus}
+d = {"Longitud de onda": x, "Mus": Mus}
 
 df12 = pd.DataFrame(d)
 
-poly = PolynomialFeatures(degree=3)
 
-x_poly = poly.fit_transform(x.reshape(-1,1))
+popt, pcov = curve_fit(fun, x, Mus)
 
-poly.fit(x_poly, Mus)
+sterr = np.sqrt(np.diag(pcov))
 
-lin2 = LinearRegression().fit(x_poly,Mus)
+a,b,c,d = round(popt[0],4),round(popt[1],4),round(popt[2],4),round(popt[3],4)
 
-x_grid0 = np.arange(min(x),max(x),5.3E-3)
-x_grid = x_grid0.reshape(len(x_grid0),1)
-y_grid = lin2.predict(poly.fit_transform(x_grid))
+a_e,b_e,c_e,d_e = round(sterr[0],4),round(sterr[1],4),round(sterr[2],4),round(sterr[3],4),
 
-df13 = {"x regression" : x_grid0, "y regression": y_grid}
-
-df13 = pd.DataFrame(df13)
+bbox = dict(boxstyle ="round",facecolor='white')
 
 #%%
 
@@ -244,21 +256,28 @@ df13 = pd.DataFrame(df13)
 Graficar A3
 """
 #eliminar estas comillas para generar graficas
-"""
+
 plt.figure()
 sns.scatterplot(data=df12, 
-    x="Espesor", y="Mus", label="Valores experimentales", color="#1ECBE1")
+    x="Longitud de onda", y="Mus", 
+    label="Valores experimentales", color="#1ECBE1")
 
-sns.lineplot(data=df13, 
-    x="x regression", y="y regression", label="Valores regresion (Grado 3)"
-    ,color="#E1341E", linestyle="dashed" )
+plt.errorbar(df12["Longitud de onda"], df12["Mus"] , yerr=Mus_error, fmt=' ' )
 
-plt.xlabel(" λ [pm] ")
+plt.plot( x_grid , fun(x_grid, *popt), 
+    label="Valores regresion (Grado 3)"
+    ,color="#E1341E", linestyle="dashed")
+plt.annotate("a = {} +/- {}; b= {} +/- {}".format(a,a_e,b,b_e), 
+             xy=(0.2, 0), bbox = bbox)
+plt.annotate("c = {} +/- {}; d= {} +/- {}".format(c,c_e,d,d_e,), 
+             xy=(0.2, -0.0002), bbox = bbox)
+
+plt.xlabel(" λ^3 [pm] ")
 plt.ylabel(" μ ")
 plt.title(" Coenficientes de absorcion vs Longitude de onda  ")
+plt.legend()
 plt.savefig(__Ipath__ + "Actividad 2-3.png", dpi=1200)
 
-"""
 #eliminar estas comillas para generar graficas
 
 # %%
@@ -266,5 +285,218 @@ plt.savefig(__Ipath__ + "Actividad 2-3.png", dpi=1200)
 """
 Actividad 3
 """
+url21="C:/Users/frawo/Documents/Programacion/Utilities/Data/Intermedio/CSV/Datos_3-1.csv"
+
+df21_v = pd.read_csv(url21,
+        names= ["Angulo","11kV","13kV","15kV","17kV","19kV","21kV"
+                ,"23kV","25kV","27kV","29kV","31kV","33kV","35kV"],
+        dtype={"Angulo":float,"11kV":float,"13kV":float,"15kV":float,"17kV":float
+               ,"19kV":float,"21kV":float,"23kV":float,"25kV":float,"27kV":float
+               ,"29kV":float,"31kV":float,"33kV":float,"35kV":float},
+        skiprows=[0,1,2]) 
+
+voltajes = ["11kV","13kV","15kV","17kV","19kV","21kV"
+        ,"23kV","25kV","27kV","29kV","31kV","33kV","35kV"]
+
+i_s = [0,3,6,9,12]
+
+theta = df21_v["Angulo"]
+
+lam = 1E9* Bragg(np.radians(theta), 2.014E-10, 1)
+
+df21_v["Longitud de onda"] = lam
+
+plt.figure()
+for i in i_s:
+    sns.scatterplot(data= df21_v,
+        x = "Longitud de onda", y=voltajes[i], 
+        label="Voltaje de {}".format(voltajes[i]), s=8)
+
+plt.xlabel("λ [nm]")
+plt.ylabel("Intensidad")
+plt.title("Intensidad vs Longitud de onda (voltaje variable)")
+
+#%%
+K = df21_v[df21_v["Angulo"] == 20.3]
+
+Is = np.zeros(len(voltajes))
+x = np.zeros(len(voltajes))
+
+for i in range(len(voltajes)):
+    x[i] = float(voltajes[i].strip("kV"))
+    Is[i] = K[voltajes[i]]
+
+h = (max(x) - min(x))/200
+
+x_grid = np.arange(min(x),max(x), h)
+
+popt, pcov = curve_fit(fun2, x, Is) 
+
+sterr = np.sqrt(np.diag(pcov))
+
+d = {"corrientes": x, "Intensidad":Is}
+
+df22_i = pd.DataFrame(d)
+bbox = dict(boxstyle ="round",facecolor='white')
+
+sns.scatterplot(data=df22_i,
+        x="corrientes", y="Intensidad",
+        label="Datos experimentales",color="#1ECBE1")
+
+plt.plot( x_grid , fun2(x_grid, *popt), 
+    label="Valores regresion (U_a)"
+    ,color="#E1341E", linestyle="dashed")
+
+plt.xlabel("Corriente +/- 0.01 (mA)")
+plt.ylabel("Intensidad +/- 1 (Imp/s)")
+
+plt.annotate("b = {} +/- {}".format(round(popt[0],2),round(sterr[0],1)), xy=(15, 8000),
+             bbox = bbox)
+
+plt.legend()
+plt.title("Intensidad vs Corriente")
+plt.savefig( __Ipath__ + "Actividad 3-1.png", dpi=1200)
+
+#%%
+
+popt, pcov = curve_fit(fun3, x, Is) 
+
+sterr = np.sqrt(np.diag(pcov))
+
+d = {"corrientes": x, "Intensidad":Is}
+
+df22_i = pd.DataFrame(d)
+bbox = dict(boxstyle ="round",facecolor='white')
+
+sns.scatterplot(data=df22_i,
+        x="corrientes", y="Intensidad",
+        label="Datos experimentales",color="#1ECBE1")
+
+plt.plot( x_grid , fun2(x_grid, *popt), 
+    label="Valores regresion (U_a-U_k varibale)"
+    ,color="#E1341E", linestyle="dashed")
+
+plt.xlabel("Corriente +/- 0.01 (mA)")
+plt.ylabel("Intensidad +/- 1 (Imp/s)")
+
+plt.annotate("b = {} +/- {}".format(round(popt[0],2),round(sterr[0],1)), xy=(15, 6000),
+             bbox = bbox)
+
+plt.legend()
+plt.title("Intensidad vs Corriente")
+plt.savefig( __Ipath__ + "Actividad 3-2.png", dpi=1200)
+
+# %%
+url22 = "C:/Users/frawo/Documents/Programacion/Utilities/Data/Intermedio/CSV/Datos_3-2.csv"
+
+df21_i = pd.read_csv(url22,
+        names= ["Angulo","0.1mA","0.2mA","0.3mA","0.4mA","0.5mA","0.6mA"
+                ,"0.7mA","0.8mA","0.9mA","1.0mA"],
+        dtype={"Angulo":float,"0.1mA":float,"0.2mA":float,"0.3mA":float
+               ,"0.4mA":float,"0.5mA":float,"0.6mA":float,"0.7mA":float
+               ,"0.8mA":float,"0.9mA":float,"1.0mA":float},
+               )
+
+corrientes = ["0.1mA","0.2mA","0.3mA","0.4mA","0.5mA","0.6mA"
+        ,"0.7mA","0.8mA","0.9mA","1.0mA"]
 
 
+i_s=[0,3,6,9]
+theta = df21_i["Angulo"]
+
+lam = 1E9* Bragg(np.radians(theta), 2.014E-10, 1)
+
+df21_i["Longitud de onda"] = lam
+
+plt.figure()
+for i in i_s:
+    sns.scatterplot(
+        data=df21_i, 
+        x="Longitud de onda", y=corrientes[i],
+        label="Corriente de {}".format(corrientes[i]), s=8
+    )
+
+plt.xlabel("λ [nm]")
+plt.ylabel("Intensidad")
+plt.title("Intensidad vs Longitud de onda (corriente variable)")
+
+#%%
+
+K = df21_i[df21_i["Angulo"] == 20.4]
+
+Is = np.zeros(len(corrientes))
+x = np.zeros(len(corrientes))
+
+for i in range(len(corrientes)):
+    x[i] = float(corrientes[i].strip("mA"))
+    Is[i] = K[corrientes[i]]
+
+
+lin3 = stats.linregress(x,Is)
+
+b = round(1E-2*lin3.slope,2)
+b_sterr = round(1E-2*lin3.stderr,2)
+
+d = {"corrientes": x, "Intensidad":Is}
+
+df22_i = pd.DataFrame(d)
+bbox = dict(boxstyle ="round",facecolor='white')
+
+sns.scatterplot(data=df22_i,
+        x="corrientes", y="Intensidad",
+        label="Datos experimentales",color="#1ECBE1")
+
+plt.plot(x, lin3.intercept + lin3.slope*x, 
+         label='regresion',linestyle="dashed",
+         color="#E1341E")
+
+plt.xlabel("Corriente +/- 0.01 (mA)")
+plt.ylabel("Intensidad +/- 1 (Imp/s)")
+
+plt.annotate("b = {} +/- {}".format(b,b_sterr), xy=(0.2, 7000),
+             bbox = bbox)
+
+plt.legend()
+plt.title("Intensidad vs Corriente")
+plt.savefig( __Ipath__ + "Actividad 3-3.png", dpi=1200)
+
+
+# %%
+
+"""
+Actividad 4
+"""
+
+url3 = "C:/Users/frawo/Documents/Programacion/Utilities/Data/Intermedio/CSV/Datos_4.csv"
+
+df31 = pd.read_csv(url3,
+        names = ["Angulo","13kV","15kV","17kV","19kV"
+                ,"21kV","23kV","25kV","27kV","29kV"],
+        dtype = {"Angulo":float,"13kV":float,"15kV":float,"17kV":float,"19kV":float
+                ,"21kV":float,"23kV":float,"25kV":float,"27kV":float,"29kV":float},
+        skiprows=[0,1,2]
+                   )
+
+voltajes = ["13kV","15kV","17kV","19kV","21kV"
+        ,"23kV","25kV","27kV","29kV"]
+
+i_s = [0,3,6,8]
+
+theta = df21_i["Angulo"]
+
+lam = 1E9* Bragg(np.radians(theta), 2.014E-10, 1)
+
+df31["Longitud de onda"] = lam
+
+#%%
+plt.figure()
+for i in i_s:
+    sns.scatterplot(
+        data=df31, 
+        x="Longitud de onda", y=voltajes[i],
+        label="Voltaje de {}".format(voltajes[i]), s=8
+    )
+plt.xlabel("λ [nm]")
+plt.ylabel("Intensidad")
+plt.title("Intensidad vs Longitud de onda (Voltaje variable))")
+# %%
